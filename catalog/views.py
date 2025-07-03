81d526262c05bc4700c700f350131b0ee054ebaf
Your Name <you@example.com>
2025-07-03 14:46:34 +0530

    Add Session Framework and User Authentication
    In this commit I have added simple Session framework for showing to the particular user how many times they have visited the webite, it uses keyId for session identification,,,,, and I have also added Authentication for Views(Only show pages if user is logged in) made a login, logout, reset password templates by uses django's inbuild settings

--- catalog/views.py
+++ catalog/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
 from .models import Book, BookInstance, Author, Genre
 
+@login_required
 def index(request):
     """View function for home page of site."""
 
@@ -17,29 +20,52 @@ def index(request):
     # The 'all()' is implied by default.
     num_authors = Author.objects.count()
 
+    num_visits = request.session.get('num_visits', 0)
+    num_visits += 1
+    request.session['num_visits'] = num_visits
+
     context = {
         'num_books': num_books,
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
         'num_genre' : num_genre,
+        'num_visits' : num_visits,
     }
 
     # Render the HTML template index.html with the data in the context variable
     return render(request, 'index.html', context=context)
 
 from django.views import generic
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-class BookListView(generic.ListView):
+class BookListView(LoginRequiredMixin, generic.ListView):
     model = Book
-    def get_queryset(self):
-        return Book.objects.filter(title__icontains='war')[:5] # Get 5 books containing the title war
-    def get_context_data(self, **kwargs):
-        # Call the base implementation first to get the context
-        context = super(BookListView, self).get_context_data(**kwargs)
-        # Create any data and add it to the context
-        context['some_data'] = 'This is just some data'
-        return context
+    paginate_by = 10
+    # def get_queryset(self):
+    #     return Book.objects.all()
+    # # (title__icontains='war')[:5] # Get 5 books containing the title war
+    # def get_context_data(self, **kwargs):
+    #     # Call the base implementation first to get the context
+    #     context = super(BookListView, self).get_context_data(**kwargs)
+    #     # Create any data and add it to the context
+    #     context['some_data'] = 'This is just some data'
+    #     return context
     
-class BookDetailView(generic.DetailView):
+class BookDetailView(LoginRequiredMixin, generic.DetailView):
     model = Book
+
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+class LoanedBooksByUserListView(LoginRequiredMixin,generic.ListView):
+    """Generic class-based view listing books on loan to current user."""
+    model = BookInstance
+    template_name = 'catalog/bookinstance_list_borrowed_user.html'
+    paginate_by = 10
+
+    def get_queryset(self):
+        return (
+            BookInstance.objects.filter(borrower=self.request.user)
+            .filter(status__exact='o')
+            .order_by('due_back')
+        )
